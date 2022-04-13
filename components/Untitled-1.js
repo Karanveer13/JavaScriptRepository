21093f5356106034acc68d6fa8dc8de296d5b5a9
gagancodes2002 <77449656+gagancodes2002@users.noreply.github.com>
2022-04-13 18:48:22 +0530

    failsafe

--- components/Untitled-1.js
+++ components/Untitled-1.js
@@ -1,13 +1,1 @@
-PMS.globals.expenses.models.map((expense) => {
-    expense.attributes.splitters.map((splitter) => {
-        if (splitter.e_splitter.friend.user.username === PMS.fn.getUsername() && expense.attributes.payer.friend.user.username !== PMS.fn.getUsername()) {
-            console.log(`${splitter.e_splitter.friend.user.username} owes ${splitter.owes} ${expense.attributes.payer.friend.user.username}`)
-            console.log(`${splitter.e_splitter.resource_uri} owes ${splitter.owes} ${expense.attributes.payer.resource_uri} in ${expense.attributes.resource_uri}`);
-            if (!_.contains(expense.attributes.payer.friend.profile, expense.attributes.settled_by)) {
-                expense.attributes.settled_by.push(PMS.globals.profile.attributes.resource_uri);
-                expense.save();
-            }
-        }
-    })
-
-});
+(!_.contains(settled_by, PMS.globals.group_friends.find((friend) => friend.attributes.friend.user.resource_uri === PMS.globals.profile.attributes.profile_user))) ? "expense-header not_settled" : "expense-header settled"
